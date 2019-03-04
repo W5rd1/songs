4fe49e858dba105593f92c3d03cf31a0c3ae17c7
Michael E <mickyfin10@hotmail.com>
2019-03-04 12:06:07 +0000

    Starting Redux

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:35" },
+    { title: "All star", duration: "3:15" },
+    { title: "Cotton Eye Joe", duration: "1:27" }
+  ];
+};
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
